cd0d818f4f44eabfadd8ea13e437b14849842375
deepak <deepak@192.168.2.5>
2017-11-30 21:34:07 +0530

    Rest with Spring

--- Rest-WebService-Spring-Implementation/src/main/java/com/deepak/restapi/controller/MessengerController.java
+++ Rest-WebService-Spring-Implementation/src/main/java/com/deepak/restapi/controller/MessengerController.java
@@ -2,6 +2,7 @@ package com.deepak.restapi.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -22,12 +23,43 @@ import com.deepak.restapi.service.UserService;
 @RequestMapping("/rest")
 public class MessengerController {
 
+	@Autowired
+	UserService userService;
+
 	/* ========= Find User Detail by User Id ============= */
 
+	/*
+	 * ResponseEntity:- is meant to represent the entire HTTP response. You can
+	 * control anything that goes into it. : status code, headers, and body.
+	 * 
+	 * @ResponseBody:- is a marker for the HTTP response body
+	 * and @ResponseStatus declares the status code of the HTTP response.
+	 * 
+	 * @ResponseStatus isn't very flexible. It marks the entire method so you
+	 * have to be sure that your handler method will always behave the same way.
+	 * And you still can't set the headers. You'd need the HttpServletResponse
+	 * or a HttpHeaders parameter.
+	 */
+
+	/*
+	 * It's a very common use case to have Controllers implement a REST API,
+	 * thus serving only JSON, XML or custom MediaType content. For convenience,
+	 * instead of annotating all your @RequestMapping methods with
+	 * 
+	 * @ResponseBody, you can annotate your Controller Class
+	 * with @RestController. @RestController is a stereotype annotation that
+	 * combines @ResponseBody and @Controller. More than that, it gives more
+	 * meaning to your Controller and also may carry additional semantics in
+	 * future releases of the framework.
+	 */
 	@RequestMapping(value = "/user/{userId}", method = RequestMethod.GET, produces = { MediaType.APPLICATION_XML_VALUE,
 			MediaType.APPLICATION_JSON_VALUE })
 	public ResponseEntity<User> findUserById(@PathVariable("userId") Integer userId) {
-		User user = UserService.userInfo(userId);
+		User user = userService.userInfo(userId);
+		if (user == null) {
+			System.out.println("User with id :: " + userId + " NOT FOUND");
+			return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
+		}
 		return new ResponseEntity<User>(user, HttpStatus.OK);
 	}
 
@@ -36,7 +68,10 @@ public class MessengerController {
 	@RequestMapping(value = "/users", method = RequestMethod.GET, produces = { MediaType.APPLICATION_XML_VALUE,
 			MediaType.APPLICATION_JSON_VALUE })
 	public ResponseEntity<List<User>> findUsers() {
-		List<User> user = UserService.usersInfo();
+		List<User> user = userService.usersInfo();
+		if (user.isEmpty()) {
+			return new ResponseEntity<List<User>>(HttpStatus.NO_CONTENT);
+		}
 		return new ResponseEntity<List<User>>(user, HttpStatus.OK);
 	}
 
@@ -46,8 +81,9 @@ public class MessengerController {
 			MediaType.APPLICATION_JSON_VALUE }, consumes = { MediaType.APPLICATION_XML_VALUE,
 					MediaType.APPLICATION_JSON_VALUE })
 	public ResponseEntity<User> createUser(@RequestBody User user) {
-		User resulltUser = UserService.createUser(user);
-		return new ResponseEntity<User>(resulltUser, HttpStatus.OK);
+
+		User resulltUser = userService.createUser(user);
+		return new ResponseEntity<User>(resulltUser, HttpStatus.CREATED);
 	}
 
 	/* ========= Updating User Detail by User Id ============= */
@@ -56,7 +92,11 @@ public class MessengerController {
 			MediaType.APPLICATION_JSON_VALUE }, consumes = { MediaType.APPLICATION_XML_VALUE,
 					MediaType.APPLICATION_JSON_VALUE })
 	public ResponseEntity<User> updateUser(@PathVariable("userId") Integer userId, @RequestBody User user) {
-		User resulltUser = UserService.updateUser(userId, user);
+		User resulltUser = userService.updateUser(userId, user);
+		if (resulltUser == null) {
+			System.out.println("User with id " + userId + " not found");
+			return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
+		}
 		return new ResponseEntity<User>(resulltUser, HttpStatus.OK);
 	}
 
@@ -64,8 +104,14 @@ public class MessengerController {
 
 	@RequestMapping(value = "/user/{userId}", method = RequestMethod.DELETE, produces = {
 			MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
-	public ResponseEntity<List<User>> deletUser(@PathVariable("userId") Integer userId) {
-		List<User> resulltUser = UserService.deleteUser(userId);
-		return new ResponseEntity<List<User>>(resulltUser, HttpStatus.OK);
+	public ResponseEntity<User> deletUser(@PathVariable("userId") Integer userId) {
+		User resulltUser = userService.userInfo(userId);
+
+		if (resulltUser == null) {
+			System.out.println("Unable to delete. User with id " + userId + " not found");
+			return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
+		}
+		userService.deleteUser(userId);
+		return new ResponseEntity<User>(HttpStatus.NO_CONTENT);
 	}
 }
